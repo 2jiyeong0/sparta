675255d6986f1acec70b39066c48d53b14082046
2jiyeong0 <2jiyeong0@gmail.com>
2022-09-13 21:14:34 +0900

    프로필 관리 기능 만들어보기
    요구조건
    
    - 사용자들의 프로필을 관리할 수 있는 클래스를 선언해주세요
    - 메소드를 호출해서 사용자의 프로필을 설정할 수 있도록 해주세요
    - 사용자의 정보를 각각 출력할 수 있는 메소드를 만들어주세요

---
+++ profile_class/profile_class.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+
+class Profile:
+    def __init__(self, *args):
+        self.profile = {
+            "name": "-",
+            "gender": "-",
+            "birthday": "-",
+            "age": "-",
+            "phone": "-",
+            "email": "-",
+
+        }
+    
+    def set_profile(self, profile):
+        self.profile = profile
+
+    def get_name(self):
+        return self.profile["name"]
+    def get_gender(self):
+        return self.profile["gender"]
+    def get_gender(self):
+        return self.profile["birthday"]
+    def get_age(self):
+        return self.profile["age"]
+    def get_phone(self):
+        return self.profile["phone"]
+    def email(self):
+        return self.profile["email"]
+
+
+profile = Profile()
+profile.set_profile({
+    "name": "lee",
+    "gender": "man",
+    "birthday": "01/01",
+    "age": 32,
+    "phone": "01012341234",
+    "email": "python@sparta.com",
+})
+
+print(profile.get_name()) # 이름 출력
+print(profile.get_gender()) # 성별 출력
+print(profile.get_gender()) # 생일 출력
+print(profile.get_age()) # 나이 출력
+print(profile.get_phone()) # 핸드폰번호 출력
+print(profile.email()) # 이메일 출력
