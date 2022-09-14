025d73f7589070b526c1ab4e164cc1b8c8a07223
2jiyeong0 <2jiyeong0@gmail.com>
2022-09-14 20:11:56 +0900

    계산기 심화
    요구조건
    
    - 클래스를 활용해 작성했던 계산기 코드를 활용해주세요
    - 기존처럼 사용자의 입력을 받고 출력하되, try / except를 활용해 사용자의 입력을 검증하는 코드를 추가해주세요
        - 두 번쨰 숫자에 0을 입력하고 나누기를 시도할 경우 “0으로 나눌 수 없습니다” 문구를 출력해주세요
        - 숫자가 아닌 다른 값을 입력했을 경우 “숫자만 입력 가능합니다” 라는 문구를 출력해 주세요

--- calc_class/calc_class.py
+++ calc_class/calc_class.py
@@ -1,8 +1,7 @@
-
 class Calc:
     def set_number(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+        self.num1 = int(num1)
+        self.num2 = int(num2)
     def plus(self):
         return self.num1 + self.num2
     def minus(self):
@@ -10,12 +9,33 @@ class Calc:
     def multiple(self):
         return self.num1 * self.num2
     def divide(self):
-        return self.num1 / self.num2
+        try:
+            return self.num1 / self.num2
+        except ZeroDivisionError: # 0으로 나누면서 에러가 발생했을 때
+            print("0으로는 나눌수 없습니다.")
 
 
-calc = Calc()
-calc.set_number(20, 10)
+num1= input("첫번째 숫자 입력: ")
+num2 = input("두번째 숫자 입력: ")
+
+try:
+    int(num1)
+
+except ValueError:  # int로 변환하는 과정에서 에러가 발생했을 떄
+    print(f"{num1}은(는) 숫자만 입력 가능합니다.")
+
+    try:
+       int(num2)
+       calc = Calc()
+       calc.set_number(num1, num2)
+    except ValueError:  # int로 변환하는 과정에서 에러가 발생했을 떄
+        print(f"{num2}은(는) 숫자만 입력 가능합니다.")
+
+
+
+
 print(calc.plus()) # 더한 값
 print(calc.minus()) # 뺀 값
 print(calc.multiple()) # 곱한 값
 print(calc.divide()) # 나눈 값
+
