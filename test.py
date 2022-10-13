5f438a82ecf92d9f1bc1b2193b83cf9ab64e1695
jiyeong <2jiyeong0@gmail.com>
2022-10-13 10:37:58 +0900

    Edit test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import torch
 import cv2
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-imgs = ['https://teamsparta.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F396579ac-a81f-4073-9409-248ba51bc5ea%2FUntitled.jpeg?table=block&id=e70a6c23-18cd-4bc6-8c46-b585967dfd56&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=&cache=v2']  # batch of images
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)    # 모델로드
 
-results = model(imgs)
+img = cv2.imread('image.jpeg')
+results = model(img)
+results.save()
 
-print(results.xyxy[0], results.xyxy[0][0][0].item())  # img1 predictions (tensor)
-print(results.pandas().xyxy[0])  # img1 predictions (pandas)
+result = results.pandas().xyxy[0].to_numpy()    # 판다스 -> 넘파이배열
+result = [item for item in result if item[6]=='person']
 
 tmp_img = cv2.imread('image.jpeg')
-cv2.rectangle(tmp_img, (int(results.xyxy[0][0][0].item()), int(results.xyxy[0][0][1].item())), (int(results.xyxy[0][0][2].item()), int(results.xyxy[0][0][3].item())), (255,255,255))
-cv2.imwrite('result1.png', tmp_img)
+print(tmp_img.shape)
+
+for i in range(len(result)):
+    cv2.rectangle(tmp_img, (int(results.xyxy[0][i][0].item()), int(results.xyxy[0][i][1].item())), (int(results.xyxy[0][i][2].item()), int(results.xyxy[0][i][3].item())), (255,255,255))
+    cv2.imwrite('result1.png', tmp_img)
+
